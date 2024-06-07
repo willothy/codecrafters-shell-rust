584e3fd30dbb505cc7fd7f7007d80470037b5b8b
Will Hopkins <willothyh@gmail.com>
2024-06-07 10:16:40 -0700

    second stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+fn main() -> std::io::Result<()> {
+    loop {
+        // Print the prompt
+        print!("$ ");
+        io::stdout().flush()?;
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input)?;
+
+        // Enter the main loop
+        match input.trim() {
+            "" => {}
+            "exit" => break,
+            cmd => {
+                println!("{cmd}: command not found");
+            }
+        }
+    }
+
+    Ok(())
 }
